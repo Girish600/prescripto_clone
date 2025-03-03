dd81d995c1953160b483c50d4c6a56605802f943
Girish kumar <Girishkumar7077@users.noreply.github.com>
2025-03-03 11:26:51 +0530

    third commit

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,14 @@ import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoutes.js';
 import doctorRouter from './routes/doctorRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// create _dirname for es module 
+
+const __filename= fileURLToPath(import.meta.url);
+const __dirname= dirname(__filename)
 
 // app config
 
@@ -14,6 +22,7 @@ const app= express();
 connectCloudinary();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "frontend/dist")))
 
 app.use('/admin',adminRouter)
 
@@ -21,8 +30,12 @@ app.use('/doctor', doctorRouter)
 
 app.use('/user', userRouter)
 
-app.get('/',(req,res)=>{
-    res.send('This is Home Page')
+// app.get('/',(req,res)=>{
+//     res.send('This is Home Page')
+// })
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 })
 
 app.listen(process.env.PORT,()=>{
